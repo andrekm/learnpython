be14a7be74e246d808257fe9a0e79c781d70bcb6
Andre de Knegt <andre.knegt@ispm.com.br>
2012-06-29 11:48:50 -0300

    number 11

---
+++ mystuff/ex11.py
@@ -0,0 +1,10 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+#',' wait for the next line after standart input
+print "how much do you weight?",
+weight = raw_input()
+
+print "So, you're %rold, %r tall and %r heavy." % (
+    age, height, weight)
