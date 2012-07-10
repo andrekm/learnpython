c23e420be5e7259fa73a6cfbaa28a1f156c2122b
Andre de Knegt <andre.knegt@ispm.com.br>
2012-07-10 15:34:44 -0300

    a few more

--- mystuff/ex17.py
+++ mystuff/ex17.py
@@ -6,8 +6,8 @@ script, from_file, to_file = argv
 print "Copying from %s to %s" % (from_file, to_file)
 
 # we could do these two on one line too, how?"
-input = open(from_file)
-indata = input.read()
+input1 = open(from_file)
+indata = input1.read()
 
 print "The input file is %d bytes long" % len(indata)
 
@@ -21,4 +21,4 @@ output.write(indata)
 print "Alright, all done."
 
 output.close()
-input.close()
+input1.close()
