18f04b25b44656f614968ecfeb2f8cb600505067
Andre de Knegt <andre.knegt@ispm.com.br>
2012-07-11 09:29:54 -0300

    more two

--- mystuff/ex20.py
+++ mystuff/ex20.py
@@ -26,7 +26,7 @@ print "Let's print three lines:"
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
 current_line = current_line + 1
