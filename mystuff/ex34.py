6f96565f2924595a1879e3925ba415b3ef43195e
ndre <knegt1@gmail.com>
2012-08-23 14:41:38 -0300

    More 3 exercises

---
+++ mystuff/ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The first animal at 1 is %s" % animals[1]
+print "The 3rd animal is %s" % animals[2]
+print "The first animal is %s" % animals[0]
+print "The animal at 3 is %s" % animals[3]
+print "The 5th animal is %s" % animals[4]
+print "The animal at 2 is %s" % animals[2]
+print "The 6th animal is %s" % animals[5]
+print "The animal at 4 is %s" % animals[4]
